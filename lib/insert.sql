80a61894aef755ed0924ad716557e79c0cefa9b3
Andrew McIndoe <amcindoe@powerhrg.com>
2021-08-18 15:39:42 -0400

    bear lab done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 5, "M", "dark brown", "hungry", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 14, "M", "black", "violent", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 8, "F", "tan", "inquisitive", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "light brown", "loner", 1);
+INSERT INTO bears VALUES (5,"Melissa", 11, "F", "brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 26, "M", "white", "friendly", 0);
+INSERT INTO bears VALUES (7,"Wendy", 11, "F", "blonde", "naive", 1);
+INSERT INTO bears VALUES (8,null, 30, "M", "black", "aggressive", 1);
